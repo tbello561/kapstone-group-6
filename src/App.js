f45eefdc3fe902fcc39e867a2b509795fdba845b
Tommy Bello <tbello4299@gmail.com>
2021-04-08 16:21:06 -0400

    added in the footer and need to finish it, also added some fetch requests that still need to be worked on

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
+import Cardio from "./views/Cardio";
+import LowerBody from "./views/LowerBody";
+import UpperBody from "./views/UpperBody";
+import Stretches from "./views/Stretches";
 
 function App() {
- return (
+  return (
     <>
       <div className="App">
         <Header />
         <Switch>
-          <Route path="/" component={TodoList} />
+          <Route exact path="/" component={TodoList} />
+          <Route exact path="/home" />
+          <Route exact path="/cardio" component={Cardio} />
+          <Route exact path="/lowerbody" component={LowerBody} />
+          <Route exact path="/upperbody" component={UpperBody} />
+          <Route exact path="/stretches" component={Stretches} />
         </Switch>
       </div>
     </>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,48 +1,38 @@
-import React from "react"
-import {useState} from "react"
-  
-  function Timer() {
-      const [timer, setTimer] = useState(60);
-      const [isOn, setIsOn] = useState(false)
-    
+import React from "react";
+import { useState } from "react";
 
-    function startTimer() {
-        setIsOn (true)
+function Timer() {
+  const [timer, setTimer] = useState(60);
+  const [isOn, setIsOn] = useState(false);
+
+  function startTimer() {
+    setIsOn(true);
     //  const timeHolder = setInterval(() =>{
     //         //setTimer(timeHolder)
     //          if(timer > 0) {
     //           setTimer(timer - 1)
-    //          } 
+    //          }
     //         }, 1000)
-    const countDown = timer > 0 && setInterval(() => setTimer(timer - 1), 1000)
+    const countDown = timer > 0 && setInterval(() => setTimer(timer - 1), 1000);
+  }
 
-        }
-             
-               
-               
+  function stopTimer() {
+    setIsOn(false);
 
-    function stopTimer() {
-        setIsOn (false)
-     
-            clearInterval(timer)
-            Timer()
-          
-    }
+    clearInterval(timer);
+    Timer();
+  }
 
-    const start = (timer > 0) ? <button onClick = {startTimer}>Start</button>:null
-    const stop = (isOn) ? <button onClick = {stopTimer}>Stop</button>:null
-    
-    
-    return(
-        <div>
-           <h4>
-               Timer:{timer} 
-           </h4>
-           {start}{stop}
-        </div>
-    )
+  const start = timer > 0 ? <button onClick={startTimer}>Start</button> : null;
+  const stop = isOn ? <button onClick={stopTimer}>Stop</button> : null;
+
+  return (
+    <div>
+      <h4>Timer:{timer}</h4>
+      {start}
+      {stop}
+    </div>
+  );
 }
-           
-      
-  
-  export default Timer;
+
+export default Timer;
